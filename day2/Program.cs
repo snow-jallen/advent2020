c54ae714694a5515a1cbb69a956b8acb6888d7a8
Jonathan Allen <jallen@snow.edu>
2020-12-01 22:11:35 -0700

    day 2 part 1

--- day2/Program.cs
+++ day2/Program.cs
@@ -1,4 +1,18 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using static System.Console;
 
-WriteLine("Hello World!");
+var passwords = from l in File.ReadAllLines("input.txt")
+                let words = l.Split('-', ' ', ':')
+                let minOccurances = int.Parse(words[0])
+                let maxOccurances = int.Parse(words[1])
+                let charInQuestion = words[2][0]
+                let password = words[4].Trim()
+                let charCount = password.Count(c => c == charInQuestion) 
+                let isValid = charCount >= minOccurances && charCount <= maxOccurances
+                where isValid == true
+                select password;
+
+WriteLine($"There are {passwords.Count()} valid passwords");
+
