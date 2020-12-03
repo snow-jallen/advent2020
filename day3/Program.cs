c679661ec16ea65c4c0bb63e8827ea75389ebe7c
Jonathan Allen <jallen@snow.edu>
2020-12-02 22:30:50 -0700

    Clean up the code a bit

--- day3/Program.cs
+++ day3/Program.cs
@@ -1,50 +1,35 @@
 ﻿using System;
 using System.IO;
 
-namespace day3
+var lines = File.ReadAllLines("input.txt");
+long ans = testPath(lines, 1, 1) *
+           testPath(lines, 3, 1) *
+           testPath(lines, 5, 1) *
+           testPath(lines, 7, 1) *
+           testPath(lines, 1, 2);
+Console.WriteLine($"Part 2 is {ans}");
+
+static long testPath(string[] lines, int xMovement, int yMovement)
 {
-    class Program
+    var col = 0;
+    long trees = 0;
+    for (int row = 0; row < lines.Length; row += yMovement)
     {
-        static void Main(string[] args)
+        var l = lines[row];
+
+        if (l[col] == '#')
         {
-            long ans = testPath(1, 1) *
-                        testPath(3, 1) *
-                        testPath(5, 1) *
-                        testPath(7, 1) *
-                        testPath(1, 2);
-            Console.WriteLine($"Part 2 is {ans}");
+            trees++;
         }
 
-        private static long testPath(int xMovement, int yMovement)
+        col += xMovement;
+
+        if (col >= l.Length)
         {
-            var lines = File.ReadAllLines("input.txt");
-            var col = 0;
-            long trees = 0;
-            for (int row = 0; row < lines.Length; row += yMovement)
-            {
-                var l = lines[row];
-
-                if (l[col] == '#')
-                {
-                    //Console.WriteLine($"\tRow {row} has a tree at position {col}");
-                    trees++;
-                }
-                else
-                {
-                    //Console.WriteLine($"\tRow {row} is tree-free at {col}");
-                }
-
-                col += xMovement;
-
-                if (col >= l.Length)
-                {
-                    //Console.WriteLine($"\t\tcol was {col} but that's longer than {l.Length} so I'm making it {col - l.Length}");
-                    col -= l.Length;
-                }
-            }
-
-            Console.WriteLine($"Moving right {xMovement} and down {yMovement} I ran into {trees} trees.");
-            return trees;
+            col -= l.Length;
         }
     }
+
+    Console.WriteLine($"Right {xMovement} / Down {yMovement} => {trees} trees");
+    return trees;
 }
