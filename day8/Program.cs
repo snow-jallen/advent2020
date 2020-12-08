357bf01eea1bc2e755888ba5d50cd4c361a7321e
Jonathan Allen <jallen@snow.edu>
2020-12-07 22:32:37 -0700

    part 2

--- day8/Program.cs
+++ day8/Program.cs
@@ -10,30 +10,69 @@ namespace day8
         static void Main(string[] args)
         {
             var lines = File.ReadAllLines("input.txt").ToList();
+            var possibleIndicies = new Queue<int>();
+            for (int i = 0; i < lines.Count; i++)
+            {
+                if (lines[i].StartsWith("jmp") || lines[i].StartsWith("nop"))
+                {
+                    possibleIndicies.Enqueue(i);
+                }
+            }
+
+            var modifiedLines = lines.ToArray();
+            while (possibleIndicies.Count >= 0)
+            {
+                if (runInstructions(modifiedLines) == 0)
+                {
+                    break;
+                }
+                modifiedLines = swapCommand(lines, possibleIndicies.Dequeue());
+            }
+        }
+
+        private static string[] swapCommand(List<string> lines, int index)
+        {
+            var copy = lines.ToArray();
+            if (copy[index].StartsWith("jmp"))
+                copy[index] = copy[index].Replace("jmp", "nop");
+            else
+                copy[index] = copy[index].Replace("nop", "jmp");
+            return copy;
+        }
+
+        private static int runInstructions(string[] lines)
+        {
             var visited = new List<int>();
             var current = 0;
             var acc = 0;
-            while(visited.Contains(current) is false)
+            while (visited.Contains(current) is false)
             {
                 visited.Add(current);
                 var parts = lines[current].Split(' ');
-                var cmd = parts[0];                
+                var cmd = parts[0];
                 var val = int.Parse(parts[1]);
-                switch(cmd)
+                switch (cmd)
                 {
                     case "nop":
-                        current ++;
-                        continue;
+                        current++;
+                        break;
                     case "jmp":
                         current += val;
-                        continue;
+                        break;
                     case "acc":
                         acc += val;
-                        current ++;
-                        continue;
+                        current++;
+                        break;
+                }
+
+                if (current >= lines.Length)
+                {
+                    Console.WriteLine($"Success - finished the instructions!  Last val of acc is {acc}");
+                    return 0;
                 }
             }
-            Console.WriteLine($"last val of acc is {acc}");
+            Console.WriteLine($"No good.  Infinite.  last val of acc is {acc}");
+            return -1;
         }
     }
 }
