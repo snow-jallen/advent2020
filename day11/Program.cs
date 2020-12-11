dbea0b91f79f2c496df9696aa40442a76df83231
Jonathan Allen <jallen@snow.edu>
2020-12-10 23:37:27 -0700

    part1

--- day11/Program.cs
+++ day11/Program.cs
@@ -1,12 +1,204 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Text;
 
 namespace day11
 {
     class Program
     {
+        const string sample_gen1 = @"
+#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##";
+
+    const string sample_gen2 = @"
+#.LL.L#.##
+#LLLLLL.L#
+L.L.L..L..
+#LLL.LL.L#
+#.LL.LL.LL
+#.LLLL#.##
+..L.L.....
+#LLLLLLLL#
+#.LLLLLL.L
+#.#LLLL.##";
+
+    const string sample_gen3 = @"
+#.##.L#.##
+#L###LL.L#
+L.#.#..#..
+#L##.##.L#
+#.##.LL.LL
+#.###L#.##
+..#.#.....
+#L######L#
+#.LL###L.L
+#.#L###.##";
+
+    const string sample_gen4 = @"
+#.#L.L#.##
+#LLL#LL.L#
+L.L.L..#..
+#LLL.##.L#
+#.LL.LL.LL
+#.LL#L#.##
+..L.L.....
+#L#LLLL#L#
+#.LLLLLL.L
+#.#L#L#.##";
+
+    const string sample_gen5 = @"
+#.#L.L#.##
+#LLL#LL.L#
+L.#.L..#..
+#L##.##.L#
+#.#L.LL.LL
+#.#L#L#.##
+..L.L.....
+#L#L##L#L#
+#.LLLLLL.L
+#.#L#L#.##";
+
+        public static Dictionary<(int row, int col), bool?> board = new ();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<(int row, int col, bool? state)> initialState = new ();
+            var items = File.ReadAllLines("input.txt");
+            for(int row = 0; row < items.Length; row++)
+            {
+                var seats = items[row];
+                for(int col = 0; col < seats.Length; col++)
+                {
+                    initialState.Add((row, col, seats[col] switch 
+                    {
+                        'L' => false,
+                        _ => null
+                    }));
+                }
+            }
+            var initialGeneration = Generation.Initialize(initialState);
+            var prevGen = initialGeneration;
+            Generation finalGen;
+            while(true)
+            {
+                var nextGen = new Generation(prevGen);
+
+                Console.Write($"{nextGen.GenerationNumber}, ");
+
+                if(nextGen.IsSameAs(prevGen))
+                {
+                    finalGen = nextGen;
+                    break;
+                }
+                prevGen = nextGen;
+            }
+            Console.WriteLine($"Part1: {finalGen.OccupiedCount}");
         }
+    }
+
+    public class Generation
+    {
+        private static int generationCount = 0;
+        public int GenerationNumber{get; init;}
+
+        public static Generation Initialize(List<(int row, int col, bool? state)> initialState)
+        {
+            var firstGeneration = new Generation();
+            foreach(var (row, col, state) in initialState)
+            {
+                firstGeneration.board[(row, col)] = state;
+            }
+            return firstGeneration;
+        }
+
+        public bool IsSameAs(Generation other) => 
+            OccupiedCount == other.OccupiedCount && board.SequenceEqual(other.board);
+
+        public int OccupiedCount => 
+            board.Values.Count(v => v.HasValue && v.Value);
+
+        private bool getValue((int row, int col) cell)
+        {
+            if(board.ContainsKey(cell))
+                return board[cell] == true;
+            return false;
+        }
+
+        private int getNeighborCount((int row, int col) cell )
+        {
+            var neighbors = new []{
+                getValue((cell.row+1, cell.col-1)),
+                getValue((cell.row+1, cell.col+0)),
+                getValue((cell.row+1, cell.col+1)),
+                getValue((cell.row, cell.col - 1)),
+                getValue((cell.row, cell.col + 1)),
+                getValue((cell.row-1, cell.col-1)),
+                getValue((cell.row-1, cell.col+0)),
+                getValue((cell.row-1, cell.col+1))                
+            };
+            var neighborCount = neighbors.Count(n => n == true);
+            return neighborCount;
+        }
+
+        //private ConcurrentDictionary<(int row, int col), bool?> board = new ();
+        private Dictionary<(int row, int col), bool?> board = new ();
+
+        private Generation()
+        {
+            GenerationNumber = generationCount++;
+        }
+
+        public Generation(Generation prev) : this()
+        {
+            //Parallel.ForEach(prev.board.Keys, cell =>
+            foreach(var cell in prev.board.Keys)
+            {
+                var neighborCount = prev.getNeighborCount(cell);
+                if(prev.board[cell] == false && neighborCount == 0) //was empty
+                    board[cell] = true;
+                else if(prev.board[cell] == true && neighborCount >= 4) //occupied w/4 neighbors
+                    board[cell] = false;
+                else
+                    board[cell] = prev.board[cell];
+            //});            
+            }
+        }
+
+        public override string ToString()
+        {
+            var str = new StringBuilder();
+            var maxRow = board.Keys.Max(c => c.row);
+            var maxCol = board.Keys.Max(c => c.col);
+
+            for(var row = 0; row < maxRow; row++)
+            {
+                var cells = board.Keys.Where(c => c.row == row).OrderBy(c => c.col);
+                foreach(var cell in cells)
+                {
+                    str.Append(board[cell] switch
+                    {
+                        true => '#',
+                        false => 'L',
+                        _ => '.'
+                    });
+                }
+                str.AppendLine();
+            }
+            return str.ToString();
+        }
+
     }
 }
