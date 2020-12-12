75e555f7022c994d284e1661500b5f89a7311e27
Jonathan Allen <jallen@snow.edu>
2020-12-11 22:55:26 -0700

    part 2

--- day12/Program.cs
+++ day12/Program.cs
@@ -17,18 +17,27 @@ namespace day12
             Dictionary<char, int> move = new (){
                 {'N', 0},{'E',0},{'S', 0},{'W',0}
             };
+            var ship = new Ship(10, 1);
+
+            Console.WriteLine($"Starting => Ship ({ship.ShipX},{ship.ShipY}) Waypoint ({ship.WaypointRelativeX},{ship.WaypointRelativeY})");
             foreach(var line in lines)
             {
                 var command = line[0];
                 var amount = int.Parse(line.Substring(1));
-                if(command == 'F')
-                    command = currentDirection;
+                //not for part 2
+                // if(command == 'F')
+                //     command = currentDirection;
 
                 switch(command)
                 {
+                    case 'F':
+                        ship.ShipX += ship.WaypointRelativeX * amount;
+                        ship.ShipY += ship.WaypointRelativeY * amount;
+                        break;
                     case 'R':
                         while(amount > 0)
                         {
+                            ship.ShiftRight();
                             currentDirection = currentDirection switch
                             {
                                 'E' => 'S',
@@ -42,6 +51,7 @@ namespace day12
                     case 'L':
                         while(amount > 0)
                         {
+                            ship.ShiftLeft();
                             currentDirection = currentDirection switch
                             {
                                 'E' => 'N',
@@ -53,19 +63,55 @@ namespace day12
                         }
                         break;
                     case 'N':
+                        ship.WaypointRelativeY += amount;
+                        move[command] += amount;
+                        break;
                     case 'E':
+                        ship.WaypointRelativeX += amount;
                         move[command] += amount;
                         break;
                     case 'S':
+                        ship.WaypointRelativeY -= amount;
                         move['N'] -= amount;
                         break;
                     case 'W':
+                        ship.WaypointRelativeX -= amount;
                         move['E'] -= amount;
                         break;
                 }
+
+                Console.WriteLine($"{line} => Ship ({ship.ShipX},{ship.ShipY}) Waypoint ({ship.WaypointRelativeX},{ship.WaypointRelativeY})");
             }
 
             Console.WriteLine($"Part1 = {Math.Abs(move['E']) + Math.Abs(move['N'])}");
+
+            Console.WriteLine($"Part2 = {ship.ManhattanDistance}");
+        }
+    }
+
+    public class Ship
+    {
+        public Ship(int waypointRelativeX, int waypointRelativeY)
+        {
+            ShipX = ShipY = 0;
+            WaypointRelativeX = waypointRelativeX;
+            WaypointRelativeY = waypointRelativeY;
+        }
+
+        public int ShipX{get;set;}
+        public int ShipY{get;set;}
+        public int ManhattanDistance => Math.Abs(ShipX) + Math.Abs(ShipY);
+        public int WaypointRelativeX { get; set;}
+        public int WaypointRelativeY { get; set;}
+
+        public void ShiftRight()
+        {
+            (WaypointRelativeX, WaypointRelativeY) = (WaypointRelativeY, WaypointRelativeX * -1);
+        }
+
+        public void ShiftLeft()
+        {
+            (WaypointRelativeX, WaypointRelativeY) = (WaypointRelativeY * -1, WaypointRelativeX);
         }
     }
 }
