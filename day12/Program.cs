870eb2e38b52f70cd6bb29ae63ac0ba014e122ca
Jonathan Allen <jallen@snow.edu>
2020-12-11 21:59:16 -0700

    prep for part 1

---
+++ day12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("sample.txt");
+        }
+    }
+}
