174024725d0939d2b90b3a3d4d4ecb95685fc058
Jonathan Allen <jallen@snow.edu>
2020-12-16 09:37:39 -0700

    finish day16 part2

--- day16/Program.cs
+++ day16/Program.cs
@@ -18,8 +18,8 @@ namespace day16
 
             var myTicket = new Ticket(lines.Skip(rules.Count + 2).First());
 
-            var nearbyTickets = from line in lines.Skip(rules.Count + 5)
-                                select new Ticket(line);
+            var nearbyTickets = (from line in lines.Skip(rules.Count + 5)
+                                select new Ticket(line)).ToList();
 
             var fields = new List<Field>();
             for(int fieldNum = 0; fieldNum < myTicket.Values.Length; fieldNum++)
@@ -31,12 +31,53 @@ namespace day16
             var invalidFields = new List<int>();
             foreach(var ticket in nearbyTickets)
             {
-                foreach(var value in ticket.Values)
+                ticket.IsValid = ticket.Values.All(v => rules.Any(r => r.IsInRange(v)));
+                invalidFields.AddRange(ticket.Values.Where(v => rules.Any(r => r.IsInRange(v)) == false));
+            }
+
+            Console.WriteLine($"Part1: {invalidFields.Sum()}");
+
+            var allTickets = nearbyTickets.Where(t=>t.IsValid).ToList();
+            allTickets.Add(myTicket);
+
+            foreach (var fieldNumber in Enumerable.Range(0, myTicket.Values.Length))
+            {
+                var allValues = allTickets.Select(t => t.Values[fieldNumber]);
+
+                foreach(var rule in rules)
                 {
-                    if (rules.Any(r => r.IsInRange(value)) is false)
-                        invalidFields.Add(value);
+                    if(rule.AreAllInRange(allValues))
+                    {
+                        rule.PossibleFieldNumbers.Add(fieldNumber);
+                    }
                 }
             }
+
+            foreach(var rule in rules.OrderBy(r=>r.PossibleFieldNumbers.Count))
+            {
+                if(rule.PossibleFieldNumbers.Count == 1)
+                {
+                    var ruleNum = rule.PossibleFieldNumbers[0];
+                    rule.ActualFieldNumber = ruleNum;
+                    rules.Where(r => r.PossibleFieldNumbers.Contains(ruleNum))
+                        .ToList()
+                        .ForEach(r => r.PossibleFieldNumbers.Remove(ruleNum));
+                }
+            }
+
+            if(rules.Any(r => r.ActualFieldNumber == null))
+            {
+                throw new Exception("unable to properly assign rules");
+            }
+
+            var departureFields = rules.Where(r => r.ruleName.Contains("departure"));
+            var product = 1L;
+            foreach(var field in departureFields)
+            {
+                Console.WriteLine($"{field.ruleName,-18}: position {field.ActualFieldNumber,2}; my ticket value={myTicket.Values[field.ActualFieldNumber.Value]}");
+                product *= myTicket.Values[field.ActualFieldNumber.Value];
+            }
+            Console.WriteLine($"Part2: {product}");
         }
     }
 
@@ -46,6 +87,14 @@ namespace day16
         {
             return (testNum >= min1 && testNum <= max1) || (testNum >= min2 && testNum <= max2);
         }
+
+        public bool AreAllInRange(IEnumerable<int> values)
+        {
+            return values.All(v => IsInRange(v));
+        }
+
+        public List<int> PossibleFieldNumbers { get; } = new ();
+        public int? ActualFieldNumber { get; set; } = null;
     }
     public record Field(int position, int max, int min, int[] values);
     public class Ticket
@@ -58,5 +107,8 @@ namespace day16
         }
 
         public int[] Values { get; }
+        public bool IsValid { get; set; }
+        public int TicketNum { get; }
+        public override string ToString() => $"{String.Join(", ", Values)} (Valid? {IsValid})";
     }
 }
