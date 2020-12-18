3a9ae7be18efb4f68fba39ec114da9d29c547fa1
Jonathan Allen <jallen@snow.edu>
2020-12-18 15:57:49 -0700

    day 18 part2

--- day18/Program.cs
+++ day18/Program.cs
@@ -11,17 +11,17 @@ namespace day18
     {
         static void Main(string[] args)
         {
-            SolveSimple("1 + 2 * 3 + 4 * 5 + 6").Should().Be(71);
+            SolveSimple("1 + 2 * 3 + 4 * 5 + 6").Should().Be(231);
             Solve("1 + (2 * 3) + (4 * (5 + 6))").Should().Be(51);
-            Solve("5 + (8 * 3 + 9 + 3 * 4 * 3)").Should().Be(437);
-            Solve("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))").Should().Be(12240);
-            Solve("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2").Should().Be(13632);
+            Solve("5 + (8 * 3 + 9 + 3 * 4 * 3)").Should().Be(1445);
+            Solve("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))").Should().Be(669060);
+            Solve("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2").Should().Be(23340);
             Console.WriteLine("looks good!");
 
             long sum = 0;
-            foreach(var line in File.ReadAllLines("input.txt"))
+            foreach (var line in File.ReadAllLines("input.txt"))
             {
-                sum+=Solve(line);
+                sum += Solve(line);
             }
             Console.WriteLine($"Part1: {sum}");
         }
@@ -32,13 +32,15 @@ namespace day18
         }
 
         public static long SolveSimple(string problem)
-        {       
+        {
+            if (problem.Contains('+'))
+                problem = ResolveAddition(problem);
             var parts = problem.Split(' ');
-            long left = int.Parse(parts[0]);
+            long left = long.Parse(parts[0]);
             for (int offset = 1; offset <= parts.Length - 2; offset += 2)
             {
                 var operation = parts[offset];
-                var right = long.Parse(parts[offset+1]);
+                var right = long.Parse(parts[offset + 1]);
                 left = operation switch
                 {
                     "+" => left + right,
@@ -50,26 +52,44 @@ namespace day18
             return left;
         }
 
+        public static string ResolveAddition(string problem)
+        {
+            var parts = problem.Split(' ').ToList();
+            var firstAddition = parts.IndexOf("+");
+            while (firstAddition > 0)
+            {
+                var left = long.Parse(parts[firstAddition - 1]);
+                var right = long.Parse(parts[firstAddition + 1]);
+
+                var ans = left + right;
+                parts.RemoveRange(firstAddition, 2);
+                parts[firstAddition-1] = ans.ToString();
+
+                firstAddition = parts.IndexOf("+");
+            }
+            return String.Join(' ', parts);
+        }
+
         public static string ResolveParenthases(string problem)
         {
             var pairs = new List<Pair>();
-            for(int i = 0; i < problem.Length; i++)
+            for (int i = 0; i < problem.Length; i++)
             {
-                if(problem[i] == '(')
+                if (problem[i] == '(')
                 {
                     pairs.Add(new Pair(i));
                 }
-                if(problem[i] == ')')
+                if (problem[i] == ')')
                 {
                     pairs.Last(p => p.Right == 0).Right = i;
-                }                
+                }
             }
-            if(pairs.Any())
+            if (pairs.Any())
             {
                 var left = pairs.Last().Left;
-                var len = pairs.Last().Right - left+1;
+                var len = pairs.Last().Right - left + 1;
                 var substr = problem.Substring(left, len);
-                var ans = SolveSimple(substr.Substring(1, substr.Length-2));
+                var ans = SolveSimple(substr.Substring(1, substr.Length - 2));
                 var newProblem = problem.Replace(substr, ans.ToString());
                 problem = ResolveParenthases(newProblem);
             }
@@ -84,7 +104,7 @@ namespace day18
         }
 
         public int Left { get; }
-        public int Right{get;set;}
+        public int Right { get; set; }
         public override string ToString() => $"{Left}, {Right}";
     }
 }
